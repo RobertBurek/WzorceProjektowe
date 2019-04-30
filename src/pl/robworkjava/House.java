ae4061226321f7eb9a8401e194f7e4e68a0e9cff
Robert Burek <robertburek@wp.pl>
2019-04-30 18:10:27 +0200

    Clear App

---
+++ src/pl/robworkjava/House.java
@@ -0,0 +1,50 @@
+package pl.robworkjava;
+
+/**
+ * Created by Robert Burek
+ */
+public class House {
+    private String adress;
+    private Integer floorsNumber;
+    private Integer doorsNumber;
+    private Integer windowsNumber;
+
+    private House(String adress, Integer floorsNumber, Integer doorsNumber, Integer windowsNumber) {
+        this.adress = adress;
+        this.floorsNumber = floorsNumber;
+        this.doorsNumber = doorsNumber;
+        this.windowsNumber = windowsNumber;
+    }
+
+    public static class HouseBuilder {
+        private String adress;
+        private Integer floorsNumber;
+        private Integer doorsNumber;
+        private Integer windowsNumber;
+
+        public HouseBuilder setAdress(String adress) {
+            this.adress = adress;
+            return this;
+        }
+
+        public HouseBuilder setFloorsNumber(Integer floorsNumber) {
+            this.floorsNumber = floorsNumber;
+            return this;
+        }
+
+        public HouseBuilder setDoorsNumber(Integer doorsNumber) {
+            this.doorsNumber = doorsNumber;
+            return this;
+        }
+
+        public HouseBuilder setWindowsNumber(Integer windowsNumber) {
+            this.windowsNumber = windowsNumber;
+            return this;
+        }
+
+        public House build() {
+            return new House(adress, floorsNumber, doorsNumber, windowsNumber);
+        }
+    }
+
+}
--- src/pl/robworkjava/Main.java
+++ src/pl/robworkjava/Main.java
@@ -3,30 +3,21 @@ package pl.robworkjava;
 public class Main {
 
     public static void main(String[] args) {
-	    //wzorzec budowniczy
-        new OldHouse("ul. Kursowa 2", 2, 3, 12);
 
-        // max 3 argumenty w metodzie/konstruktorze jeżeli są różne
-        // budowanie poprzez setery - wzorzec budowniczego
-        // new House .setAdress .setFloorsNumber . setDoorsNumber . setWindowsNumber
+        //BUILDER  -----------------------------------------------------------------------------------------------
+        //wzorzec budowniczy
 
-        //settery zwracające House dają możliwość zmutowania instacji danej klasy House
-        OldHouse fristHouse = new OldHouse()
-                .setAdress("ul. Szkolna 2")
-                .setDoorsNumber(3)
-                .setWindowsNumber(10)
-                .createHouse();
-
-        // brak możliwości stworzenia instancji bezpośredniej gdyż konstruktor jest prywatny
-        //new NewHouse("ul. Kursowa 2", 2, 3, 12);
-
-        // nowa instancja klasy NewHouse
-         NewHouse twoHouse = new NewHouse.NewHouseBuilder()
+        House firstHouse = new House.HouseBuilder()
                 .setAdress("ul. Szkolna 2")
                 .setFloorsNumber(2)
                 .setDoorsNumber(3)
                 .setWindowsNumber(10)
                 .build();
+
+        //STATYCZNE METODY WYTWÓRCZE ------------------------------------------------------------------------------
+        // from, of, valueOf, instanceOf
+        // tworzenie obiektu z innego obiektu
+
 
     }
 }
